6a4a817c3ce52b6ac31bccd898af8f6f6223e970
Zemke <florian@zemke.io>
2021-09-19 22:13:16 +0200

    Fix

--- main.py
+++ main.py
@@ -32,19 +32,16 @@ def do(M, m, row_idx, col_idx, size, W=''):
   if m is None:
     return
   nn = neighbors(M, row_idx, col_idx)
-  ww = [m] * len(nn)
   for n_idx, n_tuple in enumerate(nn.items()):
-    n = n_tuple[0]
     n_row_idx, n_col_idx = n_tuple[1]
-    ww[n_idx] += n
     mod_M = np.copy(M)
     mod_M[row_idx][col_idx] = None
     mod_M[n_row_idx][n_col_idx] = None
+    n = n_tuple[0]
     if np.count_nonzero(mod_M == None) == size:
-      print("done", W[2:] + ww[n_idx])  # Dunno why I have to substring
-      #print("done", W)
+      print("done", W + m + n)
     else:
-      do(mod_M, n, n_row_idx, n_col_idx, size, ww[n_idx] + W)
+      do(mod_M, n, n_row_idx, n_col_idx, size, W + m)
 
 
 for row_idx, row in enumerate(M):
