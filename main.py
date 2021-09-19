282e23189606e5072ac4cbd3ea52068d338f18c6
Zemke <florian@zemke.io>
2021-09-19 21:00:37 +0200

    Works for 2x2 matrix

--- main.py
+++ main.py
@@ -29,28 +29,26 @@ M = np.array([['A', 'M'],
 print(M)
 
 
-def do(M, size, W=''):
-  for row_idx, row in enumerate(M):
-    for col_idx, m in enumerate(M[row_idx]):
-      if m is None:
-        continue
-      nn = neighbors(M, row_idx, col_idx)
-      ww = [m] * len(nn)
-      for n_idx, n_tuple in enumerate(nn.items()):
-        n = n_tuple[0]
-        n_row_idx, n_col_idx = n_tuple[1]
-        ww[n_idx] += n
-        mod_M = np.copy(M)
-        mod_M[row_idx][col_idx] = None
-        mod_M[n_row_idx][n_col_idx] = None
-        print(ww[n_idx])
-        print(mod_M)
-        if np.count_nonzero(mod_M == None) == size:
-          print("done", W + ww[n_idx])
-        else:
-          print("no")
-          do(mod_M, size, ww[n_idx] + W)
+def do(M, m, row_idx, col_idx, size, W=''):
+  if m is None:
+    return
+  nn = neighbors(M, row_idx, col_idx)
+  ww = [m] * len(nn)
+  for n_idx, n_tuple in enumerate(nn.items()):
+    n = n_tuple[0]
+    n_row_idx, n_col_idx = n_tuple[1]
+    ww[n_idx] += n
+    mod_M = np.copy(M)
+    mod_M[row_idx][col_idx] = None
+    mod_M[n_row_idx][n_col_idx] = None
+    if np.count_nonzero(mod_M == None) == size:
+      print("done", W[2:] + ww[n_idx])  # Dunno why I have to substring
+      #print("done", W)
+    else:
+      do(mod_M, n, n_row_idx, n_col_idx, size, ww[n_idx] + W)
 
 
-do(M, M.size)
+for row_idx, row in enumerate(M):
+  for col_idx, m in enumerate(M[row_idx]):
+    do(M, m, row_idx, col_idx, M.size)
 
